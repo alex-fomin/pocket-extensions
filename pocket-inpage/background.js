e2ea925599d473420d6ea166ad422fce06beb997
Alex Fomin <ShurickFomin@gmail.com>
2013-06-02 17:35:24 +0300

    Added IndexDB as server emulator. Use deferred to emulate async IO

--- pocket-inpage/background.js
+++ pocket-inpage/background.js
@@ -1,53 +1,39 @@
-var urls=[];
+var pocket = new PocketApi();
 
-function isAlreadyAdded(tab) {
-    return _.contains(urls,tab.url);
+function onActionClick(tab) {
+    console.log('onClick');
+
+    pocket.isAdded(tab.url)
+        .then(function (isAdded) {
+            var deffer = isAdded ? pocket.remove(tab.url) : pocket.add(tab.url);
+            return deffer.then(function () {
+                return isAdded;
+            });
+        })
+        .done(function (isAdded) {
+            showIcon(tab.id, !isAdded)
+        });
 }
 
-function showIcon(tab, tabId, icons) {
-    chrome.pageAction.setIcon({
-        tabId:tabId,
-        path:icons
-    });
-}
+
 function onTabUpdate(tabId, changeInfo, tab) {
     chrome.pageAction.show(tabId);
-    if (isAlreadyAdded(tab))
-        showIcon(tab, tabId, {"19":"images/red-19.png"});
-    else
-        showIcon(tab, tabId, {"19":"images/black-19.png"});
+    pocket
+        .isAdded(tab.url)
+        .done(function (isAdded) {
+            showIcon(tabId, isAdded);
+        });
 }
 
-function remove(tab) {
-    var index = urls.indexOf(tab.url);
-    if (index>=0)
-        urls.splice(index,1);
-}
-function add(tab) {
-    var index = urls.indexOf(tab.url);
-    if (index<0)
-        urls.push(tab.url);
-}
-function onActionClick(tab){
-    if (isAlreadyAdded(tab))
-        remove(tab);
-    else
-        add(tab);
-    onTabUpdate(tab.id, null, tab);
+function showIcon(tabId, isAdded) {
+    var addedIcon = {"19": "images/red-19.png"};
+    var notAddedIcon = {"19": "images/black-19.png"};
+    chrome.pageAction.setIcon({
+        tabId: tabId,
+        path: isAdded ? addedIcon : notAddedIcon
+    });
 }
 
 
-
-// Listen for any changes to the URL of any tab.
 chrome.tabs.onUpdated.addListener(onTabUpdate);
 chrome.pageAction.onClicked.addListener(onActionClick);
-
-
-/*function(tab) {
-    // No tabs or host permissions needed!
-    console.log('Turning ' + tab.url + ' red!');
-    chrome.tabs.executeScript({
-        code: 'document.body.style.backgroundColor="red"'
-    });
-});
- */
