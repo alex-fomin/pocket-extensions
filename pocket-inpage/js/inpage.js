9011c8eed9c187a67c77b22645b97334a400fd07
Alex Fomin <ShurickFomin@gmail.com>
2013-06-17 22:21:30 +0300

    Add context menu action

--- pocket-inpage/js/inpage.js
+++ pocket-inpage/js/inpage.js
@@ -1,8 +1,8 @@
 require(['js/communicate'], function (pocket) {
 
-    var addedIcon = {"19" : "images/added-19.png"};
-    var notAddedIcon = {"19" : "images/notAdded-19.png"};
-    var unknownIcon = {"19" : "images/unknown-19.png"};
+    var addedIcon = "images/added-18.png";
+    var notAddedIcon = "images/notAdded-18.png";
+    var unknownIcon = "images/unknown-18.png";
 
     function showPocketStatus(tab, added) {
         chrome.pageAction.setIcon({
@@ -17,17 +17,18 @@ require(['js/communicate'], function (pocket) {
     }
 
     var rotating = true;
+
     function startRotating(tabId) {
 
         var images = [
-            'images/loader1-19.png',
-            'images/loader2-19.png',
-            'images/loader3-19.png',
-            'images/loader4-19.png',
-            'images/loader5-19.png',
-            'images/loader6-19.png',
-            'images/loader7-19.png',
-            'images/loader8-19.png'
+            'images/loader/1.png',
+            'images/loader/2.png',
+            'images/loader/3.png',
+            'images/loader/4.png',
+            'images/loader/5.png',
+            'images/loader/6.png',
+            'images/loader/7.png',
+            'images/loader/8.png'
         ];
 
         var current = 0;
@@ -37,21 +38,23 @@ require(['js/communicate'], function (pocket) {
             if (rotating) {
                 chrome.pageAction.setIcon({tabId : tabId, path : images[current]});
                 current = (current + 1) % images.length;
-                setTimeout(rotate, 300);
+                setTimeout(rotate, 150);
             }
         }
 
         rotate();
     }
 
-    function stopRotating(){
+    function stopRotating() {
         rotating = false;
     }
 
 
     function onActionClick(tab) {
+        var url = tab.url;
+
         startRotating(tab.id);
-        pocket.find(tab.url)
+        pocket.find(url)
             .then(function (item) {
                 var deffer = item ? pocket.remove(tab.url) : pocket.add(tab.url);
                 return deffer.then(function () {
@@ -70,6 +73,7 @@ require(['js/communicate'], function (pocket) {
             chrome.pageAction.hide(tabId);
         }
         else {
+
             chrome.pageAction.show(tabId);
             pocket.find(tab.url)
                 .done(function (item) {
@@ -88,4 +92,24 @@ require(['js/communicate'], function (pocket) {
 
     chrome.tabs.onUpdated.addListener(onTabUpdate);
     chrome.pageAction.onClicked.addListener(onActionClick);
+
+    function add(tab, url) {
+        startRotating(tab.id);
+        pocket.add(url)
+            .done(function () {
+                stopRotating();
+                showPocketStatus(tab, true);
+            });
+    }
+
+    var menuId = chrome.contextMenus.create({
+        type                : 'normal',
+        title               : 'Add to Pocket',
+        contexts            : ["page", "frame", "link"],
+        documentUrlPatterns : ["http://*/*", "https://*/*"],
+        targetUrlPatterns   : ["http://*/*", "https://*/*"],
+        onclick             : function (info, tab) {
+            add(tab, info.linkUrl || info.pageUrl);
+        }
+    });
 });
